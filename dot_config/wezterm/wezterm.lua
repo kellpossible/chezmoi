34e5509e4090da6de685449cf5bc4120ebde72ad
Luke Frisken <l.frisken@gmail.com>
2021-12-26 20:57:24 +0400

    Add wezterm configuration

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm';
+
+return {
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  color_scheme = "Monokai Remastered",
+  skip_close_confirmation_for_processes_named = {
+    "bash", "sh", "zsh", "fish", "tmux"
+  },
+  window_close_confirmation = "NeverPrompt",
+  keys = {
+    {key="w", mods="CTRL|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=false}}},
+    {key="\t", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
+    {key="\t", mods="CTRL", action=wezterm.action{ActivateTabRelative=1}},
+  },
+  check_for_updates = false,
+}
