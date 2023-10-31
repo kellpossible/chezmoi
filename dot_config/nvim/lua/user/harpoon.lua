9d277f60a7e956ba087e6f55e7ff666d610b8408
Luke Frisken <luke@partly.com>
2023-10-31 16:04:11 +0300

    Add harpoon config

---
+++ dot_config/nvim/lua/user/harpoon.lua
@@ -0,0 +1,9 @@
+require("harpoon").setup({
+  global_settings = {
+    -- set marks specific to each git branch inside git repository
+    mark_branch = true,
+  },
+  menu = {
+    width = 80,
+  },
+})
