9f148134f8b6c7b611d9f19ccbd831e05b482398
Luke Frisken <luke@partly.com>
2024-02-13 13:58:07 +0400

    Updated neovide config

--- dot_config/nvim/lua/user/neovide.lua
+++ dot_config/nvim/lua/user/neovide.lua
@@ -1,7 +1,8 @@
-vim.g.neovide_window_position_animation_length = 0
+-- vim.g.neovide_window_position_animation_length = 0
 vim.g.neovide_window_floating_opacity = 1
 vim.g.neovide_window_floating_blur = 0
 vim.g.neovide_floating_blur = 0
 vim.g.neovide_floating_opacity = 1
 vim.g.neovide_confirm_quit = 1
+vim.g.neovide_scroll_animation_length = 0.1
 -- vim.g.neovide_cursor_vfx_mode = "pixiedust"
