3e7890af4c58e2bdd6507d19f93cd8b700e0dd54
Luke Frisken <l.frisken@gmail.com>
2024-09-03 12:31:42 +1000

    neovim: disable lazy.nvim auto checking

--- dot_config/nvim/lua/user/lazy.lua
+++ dot_config/nvim/lua/user/lazy.lua
@@ -27,10 +27,5 @@ require("lazy").setup({
     -- import your plugins
     { import = "user.plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
 })
 
